520d4b50ccdab7451d5c82ea2dc299891503476e
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-02-19 19:40:06 +0300

    task done

--- hw-2.js
+++ hw-2.js
@@ -8,3 +8,14 @@ alert(year);
 //3
 let text = "Brendan Eich";
 alert (text);
+//4
+let x = 10;
+let y = 2;
+let sum = x + y;
+alert (sum);
+let sub = x - y;
+alert (sub);
+let multipl = x * y;
+alert (multipl);
+let division = x/y;
+alert (division);
