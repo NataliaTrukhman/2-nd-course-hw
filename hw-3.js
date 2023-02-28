365a9bdbc896baabaeaa405a2318abf83a9bcdd0
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-02-28 23:31:45 +0300

    5 done

--- hw-3.js
+++ hw-3.js
@@ -25,3 +25,31 @@ if (d > 100 || e > 100) {
 let a = Number ('2');
 let b = Number ('3');
 alert(a + b)
+
+//5
+let monthNumber = Number(prompt('Введите номер месяца'));
+switch (monthNumber) {
+    case 12:
+    case 1 :
+    case 2 :
+        console.log ('зима');
+        break;
+    case 3 :
+    case 4 :
+    case 5 :
+            console.log ('весна');
+            break;
+    case 6 :
+    case 7 :
+    case 8:
+            console.log ('лето');
+            break;
+    case 9 :
+    case 10 :
+    case 11 :
+                console.log ('осень');
+                break;
+    default:console.log ('нет такого');
+        break;
+}
+
