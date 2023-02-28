b4b86aa167ccaad0a70b2791c3ae23d7af4e8631
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-02-28 22:03:24 +0300

    1 DONE

---
+++ hw-3.js
@@ -0,0 +1,9 @@
+//1
+let password = 'пароль';
+let ask = prompt ('Введите пароль');
+if ( ask === password ) {
+    console.log ('Пароль введен верно');
+} else {
+    console.log ('Пароль введен неправильно');
+}
+//2
