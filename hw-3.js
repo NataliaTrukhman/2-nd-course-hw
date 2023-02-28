7063b6651f88bb763aaa5249c91c50332be39389
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-02-28 22:22:20 +0300

    3 done

--- hw-3.js
+++ hw-3.js
@@ -9,7 +9,15 @@ if ( ask === password ) {
 //2
 let c = 5;
 if (c > 0 && c < 10 ) {
-    console.log ('Верно')
+    console.log ('Верно');
 } else {
-    console.log ('Неверно')
+    console.log ('Неверно');
+}
+//3
+let d = 777;
+let e = 7;
+if (d > 100 || e > 100) {
+    console.log ('Верно');
+} else {
+    console.log ('Неверно');
 }
