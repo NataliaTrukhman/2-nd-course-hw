09e77aede5d58fffb21bce9b52abc435c316df9e
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-03-06 20:18:11 +0300

    5  done

--- hw-5.js
+++ hw-5.js
@@ -35,4 +35,35 @@ function degree(x) {
 }
 alert(degree(x));
 
+//4
 
+let age = Number(prompt('Сколько вам лет?'));
+function userAge(age) {
+    if (age < 0) {      //нужно ли  и как если нужно добавить проверку, если введет не числовые значения
+    alert("Вы ввели неправильное значение");
+} else if (age <= 12) {
+    alert("Привет, друг!");
+} else if (age >= 13) {
+        alert("Добро пожаловать!");
+}
+}
+userAge(age);
+
+
+
+//5
+
+let m = prompt('Введите первое число');
+let n = prompt('Введите первое число');
+
+function examination(m, n) {
+    
+if (isNaN(m) || isNaN(n)) {
+         console.log('Одно или оба значения не являются числом');
+ } else {
+         console.log(m * n);
+           }
+ }
+ examination(m, n);
+
+//6
