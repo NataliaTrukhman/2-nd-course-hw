47d10e5d7c642109f8d18c9a9e089296f28f86ea
natalia-trukhman <119609124+natalia-trukhman@users.noreply.github.com>
2023-03-11 15:39:20 +0300

    //5 почему если короче: выводит другой результат

---
+++ hw-6.js
@@ -0,0 +1,47 @@
+//1
+// const numbers = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < numbers.length; i++ ) {
+//     console.log(numbers[i]);
+//     if(numbers[i] == 10) break;
+    
+// };
+ 
+//2
+// const numb = [1, 5, 4, 10, 0, 3];
+// console.log(numb.indexOf(4));
+
+
+ //3
+//  const numbs = [1, 3, 5, 10, 20];
+//  console.log(numbs.join(' '));
+
+//4
+// let arr = [];
+// for (let i = 0; i < 4; i++)
+// {
+//     arr[i] = [];
+//     for (let j = 0; j <= i; j++)
+//     {
+//         arr[i][j] = j + 1;
+//     }
+// }
+// console.log(arr);
+
+// const arr = [];
+
+// for (const arrIn = 0; arrIn < 3; arrIn++) {
+// arr [arrIn] = [];
+// for (const item = 0; item < 3; item++) {
+// arrIn = [item];
+//  }
+// }
+// console.log(arr);
+
+//5
+// const arr = [1, 1, 1]; //методом push
+// arr.push (2, 2, 2);
+// console.log(arr); // почему если записать короче: console.log(arr.push(2, 2, 2)); - выводит другой результат
+
+// const arr = [1, 1, 1]; // методом contact 
+// console.log(arr.concat([2, 2, 2]));  
+
